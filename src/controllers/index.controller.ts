c88aa449f81d47a10f461dfbe44ac2a4b608412d
Robertlasca <roberto.lasca123@gmail.com>
2022-10-23 21:42:08 -0500

    Configuration project

---
+++ src/controllers/index.controller.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from "express";
+
+export function indexResponse(req: Request, res: Response) {
+  const data = { title: "Programacion web" };
+  return res.render("index", data);
+}
+
+export function mainResponse(req: Request, res: Response): Response {
+  const html = `
+  <h1>Soy un titulo</h1>
+  <input>`;
+
+  return res.send(html);
+}
+
+export function sumaParametrosResponse(req: Request, res: Response): Response {
+  const argumentos = req.query;
+  const { numeroA, otroNumero } = argumentos;
+  if (numeroA !== undefined && otroNumero !== undefined) {
+    const suma = Number(numeroA) + Number(otroNumero);
+    return res.status(200).send(`Soy la suma ${suma}`);
+  } else {
+    return res.status(400).send("No puedo Marta");
+  }
+}
+
+export function sumaParametrosFijosResponse(req: Request, res: Response): Response {
+  const argumentos = req.params;
+
+  const { numeroA, numeroB } = argumentos;
+  if (numeroA !== undefined && numeroB !== undefined) {
+    const suma = Number(numeroA) + Number(numeroB);
+    return res.status(200).send(`Soy la suma ${suma}`);
+  } else {
+    return res.status(400).send("No puedo Marta");
+  }
+}
---
+++ src/routes/example.route.ts
@@ -0,0 +1,7 @@
+import {Router} from "express";
+import {createExample} from "../../controllers/example.controller"
+const exampleRouter: Router = Router();
+
+exampleRouter.get("/create/:idStatus/:status/:description",createExample);
+
+export default exampleRouter;
